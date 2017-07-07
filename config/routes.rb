d35713a8ddb9deacd840bc4dd6340d4c7131f4b1
Cuong-FrontEnd <cuong@codeforcevina.com>
2017-07-07 17:19:57 +0700

    |

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :regions
+  resources :places
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170707095758_create_places.rb
@@ -0,0 +1,14 @@
+class CreatePlaces < ActiveRecord::Migration[5.1]
+  def change
+    create_table :places do |t|
+    t.string :name
+      t.string :location_id
+      t.decimal :rating
+      t.decimal :latitude
+      t.decimal :longitude
+      t.references :region, index: true
+
+      t.timestamps
+    end
+  end
+end
