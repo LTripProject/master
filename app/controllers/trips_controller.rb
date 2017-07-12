b661a02b0dad7bb9e95cdf26791cd98872353d53
Cuong-FrontEnd <cuong@codeforcevina.com>
2017-07-12 11:02:13 +0700

    add trip controler function

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,31 @@
 class TripsController < ApplicationController
+    before_action :require_login, except: [:show]
+
+
+  def show
+    unless @trip.user_ids.include?(current_user_id)
+      
+    end
+  end
+
+  def edit
+    if @trip.id != current_trip
+      set_current_trip(@trip.id)
+    end
+  end 
+
+  def create
+    @trip = Trip.new(trip_params)
+    @created = @trip.save
+    @trip.user_trips.create(user_id: current_user_id) if @created
+  end
+
+  def update
+    @trip.departure = Region.find_by(name: params[:trip][:departure])
+    @updated = @trip.update_attributes(trip_update_params)
+  end
+
+  def trip_params
+    params.require(:trip).permit(:title, :expected_budget)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_user_id
+    session[:user_id]
+  end
+
+  def set_current_trip(trip_id)
+    session[:current_trip] = trip_id
+  end
+  def current_trip
+    session[:current_trip]
+  end
+
+  def clear_user_id
+    session[:user_id] = nil
+  end
+  
 end
