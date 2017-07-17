3afc93f0fc944d6ff5eaaf86fcb1d279b34c1296
danhvo <danh.vo@4tellus.com>
2017-07-17 17:10:22 +0700

    create schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,17 @@
 class SchedulesController < ApplicationController
 	def new
-		@schedule = Schedule.new
+		@trip = Trip.find(:trip_id)
 		
+	end
+
+	def create
+		@trip = Trip.find(:trip_id)
+		schedule = @trip.build(schedule_params)
+		if schedule.save then
+			
+	end
+
+	def
+		schedule_params.require(:schedule).permit(:title)
 	end
 end
