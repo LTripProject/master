430b182fdaa497087cdeaa4f0b6f08a0df575e81
Cuong-FrontEnd <cuong@codeforcevina.com>
2017-08-09 16:53:38 +0700

    Update Service

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,7 @@
 class TripsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :confirm_invite]
   before_action :set_trip, except: [:index, :new, :create, :join, :update_video_link]
-  before_action :check_permission, only: [:edit, :update, :destroy, :invite, :upload_gallery]
+  before_action :check_permission, only: [:edit, :update, :destroy, :invite, :upload_gallery, :clone]
 
   def index
     @trips = Trip.includes(:users).all
@@ -23,6 +23,11 @@ class TripsController < ApplicationController
 
   def edit
     set_current_trip(@trip)
+  end
+
+   def clone
+    new_trip = CloneService.new(@trip, current_user).execute
+    redirect_to edit_trip_path(new_trip)
   end
 
 
