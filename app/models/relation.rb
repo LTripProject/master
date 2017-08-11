b71072bd11bbebbd0f09e9b87514033cacc68d2a
The Tien <tiendt2311@gmail.com>
2017-08-12 00:00:19 +0700

    before write relation ship

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,5 +1,6 @@
 class Relation < ApplicationRecord
   belongs_to :user
-  
+  belongs_to :friend, class_name: "User", foreign_key: :target_id
 
+  validates :user_id, :friend_id, presence: true
 end
